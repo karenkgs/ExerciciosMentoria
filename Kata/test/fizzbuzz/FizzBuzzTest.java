29db8f5299a1d0a8aab3b7e55b6413460caa4c4c
Anki <Anki@Hyperion>
2017-03-31 17:17:44 -0300

    Parte 2 do exercício

--- Kata/test/fizzbuzz/FizzBuzzTest.java
+++ Kata/test/fizzbuzz/FizzBuzzTest.java
@@ -15,7 +15,7 @@ public class FizzBuzzTest {
     
     private final FizzBuzz fizzBuzz = new FizzBuzz();
     private String resultado;
-
+   
     @Test
     public void deveRetornarString1() {
         resultado = fizzBuzz.gerarResultado(1);
@@ -35,4 +35,21 @@ public class FizzBuzzTest {
                 resultado, equalTo("1\n2\nFizz\n"));
     }
     
+    @Test
+    public void deveRetornarBuzzParaNumeroDivisivelPor5() {
+        resultado = fizzBuzz.gerarResultado(5);
+        assertThat("Deve conter Buzz" ,resultado, equalTo("1\n2\nFizz\n4\nBuzz\n"));
+    }
+    
+    @Test
+    public void deveRetornarFizzBuzzParaNumeroDivisivelPor3e5(){
+        resultado = fizzBuzz.gerarResultado(15);
+        assertThat("Deve conter BuzzFizz", resultado, equalTo("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n"));
+    }
+    
+    @Test
+    public void deveRetornarFizzParaNumeroContendo3(){
+        resultado = fizzBuzz.gerarResultadoEstagio2(13);
+        assertThat("Deve conter BuzzFizz", resultado, equalTo("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\nFizz\n"));
+    }
 }
