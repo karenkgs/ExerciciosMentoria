011a2a611a204d2483a3906e3a1bed92070a7ef9
Karen <karenkgs@gmail.com>
2017-04-02 12:47:19 -0300

    Methods refactoring, for OR and AND cases

--- KataRefactoring/src/fizzBuzz/FizzBuzz.java
+++ KataRefactoring/src/fizzBuzz/FizzBuzz.java
@@ -23,11 +23,11 @@ public class FizzBuzz {
     public String generateNumbersListWithNewRequirements(int lastNumber){
                 String numberList = "";
         for(int cont = 1; cont <= lastNumber; cont++){
-            if(getIfNumberIsAMultipleOf3And5(cont) || getIfNumberHas3And5OnIt(cont)) {
+            if(getIfNumberIsAMultipleOf3And5OrHas3And5OnIt(cont)) {
                 numberList += "FizzBuzz";
-            } else if(getIfNumberIsAMultipleOf3(cont) || getIfNumberHas3OnIt(cont)){
+            } else if(getIfNumberIsAMultipleOf3OrHas3OnIt(cont)){
                 numberList += "Fizz";
-            } else if(getIfNumberIsAMultipleOf5(cont) || getIfNumberHas5OnIt(cont)) {
+            } else if(getIfNumberIsAMultipleOf5OrHas5OnIt(cont)) {
                 numberList += "Buzz";
             } else {
                 numberList += cont;
@@ -46,10 +46,6 @@ public class FizzBuzz {
         return (number % 5) == 0;
     }
     
-    private boolean getIfNumberIsAMultipleOf3And5(int number){
-        return ((number % 3) == 0 && (number % 5) == 0);
-    }
-    
     private boolean getIfNumberHas3OnIt(int number){
         return String.valueOf(number).contains("3");
     }
@@ -58,8 +54,25 @@ public class FizzBuzz {
         return String.valueOf(number).contains("5");
     }
     
+    
+    private boolean getIfNumberIsAMultipleOf3And5(int number){
+        return ((number % 3) == 0 && (number % 5) == 0);
+    }
+        
     private boolean getIfNumberHas3And5OnIt(int number){
         return (String.valueOf(number).contains("3") && String.valueOf(number).contains("5"));
     }
     
+    
+    private boolean getIfNumberIsAMultipleOf3And5OrHas3And5OnIt(int number){
+        return (getIfNumberIsAMultipleOf3And5(number) || getIfNumberHas3And5OnIt(number));
+    }
+    
+    private boolean getIfNumberIsAMultipleOf3OrHas3OnIt(int number){
+        return (getIfNumberIsAMultipleOf3(number) || getIfNumberHas3OnIt(number));
+    }
+    
+    private boolean getIfNumberIsAMultipleOf5OrHas5OnIt(int number){
+        return (getIfNumberIsAMultipleOf5(number) || getIfNumberHas5OnIt(number));
+    }
 }
