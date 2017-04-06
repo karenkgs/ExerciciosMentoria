93d69f43cc7a5296d87a4781175039e057275e85
Karen Garcia <karenkgs@gmail.com>
2017-04-06 11:23:24 -0300

    Mooc Fi project

--- MoocFiSmileys/src/Smileys.java
+++ MoocFiSmileys/src/Smileys.java
@@ -3,9 +3,48 @@ public class Smileys {
 
     public static void main(String[] args) {
         // Test your method at least with the following
-        // printWithSmileys("Method");
-        // printWithSmileys("Beerbottle");
-        // printWithSmileys("Interface");
+         printWithSmileys("Mikael");
+         printWithSmileys("Matti");
+         printWithSmileys("Arto");
     }
 
+    private static void printWithSmileys(String characterString){
+        System.out.print(generateSmilesListWithCharacterStringSurroundedBySmiles(characterString));
+    }
+    
+    private static String generateSmilesListWithCharacterStringSurroundedBySmiles(String characterString){
+        String smilesListWithCharacterStringSurroundedBySmiles = "";
+        int characterStringLength = characterString.length();
+        smilesListWithCharacterStringSurroundedBySmiles += generateSmilesList(characterStringLength+1);
+        smilesListWithCharacterStringSurroundedBySmiles += generateCharacterStringSurroundedBySmiles(characterString);
+        smilesListWithCharacterStringSurroundedBySmiles += generateSmilesList(characterStringLength+1);
+        
+        return smilesListWithCharacterStringSurroundedBySmiles;
+    }
+    
+    private static String generateSmilesList(int smiles){
+        String smilesList = "";
+        for(int cont = 0; cont<smiles; cont++){
+            smilesList += ":)";
+        }
+        smilesList += "\n";
+        
+        return smilesList;
+    }
+    
+    private static String generateCharacterStringSurroundedBySmiles(String characterString){
+        String characterStringSurroundedBySmiles = "";
+        characterStringSurroundedBySmiles += ":) " + characterString;
+        if(getIfACharacterStringLengthIsOdd(characterString.length())){
+            characterStringSurroundedBySmiles += " ";
+        }
+        characterStringSurroundedBySmiles += " :)\n";
+        
+        return characterStringSurroundedBySmiles;
+    }
+    
+    private static boolean getIfACharacterStringLengthIsOdd(int characterStringLength){
+            return (characterStringLength % 2) != 0;
+    }
+    
 }
